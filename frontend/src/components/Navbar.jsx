153af5f22fced90d7bdafa4d66ab154b9b1cc978
shreyasKU31 <shreyasku31@gmail.com>
2024-10-25 19:20:06 +0530

    frontend-finish

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import {assets} from '../assets/assets.js'
-import { useState } from 'react'
-import {NavLink, useNavigate} from 'react-router-dom'
+import React from "react";
+import { assets } from "../assets/assets.js";
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
 
-  const [showMenu, setShowMenu] = useState(false)
-  const [token, setToken] = useState(true)
+  const [showMenu, setShowMenu] = useState(false);
+  const [token, setToken] = useState(true);
 
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
-      <img onClick={() => navigate('/')} src={assets.logo} className="w-44 cursor-pointer" alt="logo" />
+      <img
+        onClick={() => navigate("/")}
+        src={assets.logo}
+        className="w-44 cursor-pointer"
+        alt="logo"
+      />
       <ul className="hidden md:flex items-start gap-5 font-medium">
         <NavLink to="/">
           <li className="py-1">Home</li>
@@ -49,21 +54,73 @@ const Navbar = () => {
                 >
                   My Appointments
                 </p>
-                <p onClick={()=>setToken(false)} className="hover:text-black cursor-pointer">Logout</p>
+                <p
+                  onClick={() => setToken(false)}
+                  className="hover:text-black cursor-pointer"
+                >
+                  Logout
+                </p>
               </div>
             </div>
           </div>
         ) : (
           <button
-            onClick={() => {navigate("/login"); setToken(true)}}
+            onClick={() => {
+              navigate("/login");
+              setToken(true);
+            }}
             className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block"
           >
             Create Account
           </button>
         )}
+
+        <img
+          onClick={() => setShowMenu(true)}
+          className="w-6 md:hidden"
+          src={assets.menu_icon}
+          alt=""
+        />
+        {/* ---------- Mobile Menue ---------- */}
+        <div
+          className={`${
+            showMenu ? "fixed w-full" : "h-0 w-0"
+          } md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}
+        >
+          <div className="flex items-center justify-between px-5 py-6">
+            <img className="w-36" src={assets.logo} alt="" />
+            <img className="w-7" src={assets.cross_icon} alt="" />
+          </div>
+          <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+            <NavLink
+              onClick={() => setShowMenu(false)}
+              to="/"
+            >
+              <p className="px-4 py-2 rounded inline-block">Home</p>
+            </NavLink>
+            <NavLink
+              onClick={() => setShowMenu(false)}
+              to="/doctors"
+            >
+              <p className="px-4 py-2 rounded inline-block">All Doctors</p>
+            </NavLink>
+            <NavLink
+              onClick={() => setShowMenu(false)}
+              to="/about"
+            >
+              <p className="px-4 py-2 rounded inline-block">About</p>
+            </NavLink>
+            <NavLink
+              onClick={() => setShowMenu(false)}
+              to="/contact"
+            >
+              <p className="px-4 py-2 rounded inline-block">Contact</p>
+            </NavLink>
+          </ul>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
